6660c67ea396b479047808e7c4302e7e07a2e160
bboheekim <bohee813@naver.com>
2024-01-16 03:15:03 +0900

    first commit

---
+++ script/script.js
@@ -0,0 +1,81 @@
+
+$(function(){
+    // 디자인워크 브랜딩
+    $(".popup-Bottena").click(function(){
+        $(".popup-d1").show();
+    });
+    $(".popup-closed1").click(function(){
+        $(".popup-d1").hide();
+    });
+    // 디자인워크 공모전
+    $(".popup-Bottenb").click(function(){
+        $(".popup-d2").show();
+    });
+    $(".popup-closed2").click(function(){
+        $(".popup-d2").hide();
+    });
+    // 코로나행성
+    $(".popup-Btna").click(function(){
+        $(".popup-p1").show();
+    });
+
+    $(".popup-closea").click(function(){
+        $(".popup-p1").hide();
+    });
+    // 세계경제
+    $(".popup-Btnb").click(function(){
+        $(".popup-p2").show();
+    });
+
+    $(".popup-closeb").click(function(){
+        $(".popup-p2").hide();
+    });
+    // 연쇄전쟁
+    $(".popup-Btnc").click(function(){
+        $(".popup-p3").show();
+    });
+
+    $(".popup-closec").click(function(){
+        $(".popup-p3").hide();
+    });
+    // 최초의 코로나
+    $(".popup-Btnd").click(function(){
+        $(".popup-p4").show();
+    });
+
+    $(".popup-closed").click(function(){
+        $(".popup-p4").hide();
+    });
+    // 마약
+    $(".popup-Btne").click(function(){
+        $(".popup-p5").show();
+    });
+
+    $(".popup-closee").click(function(){
+        $(".popup-p5").hide();
+    });
+    // 플랜비
+    $(".popup-Btnf").click(function(){
+        $(".popup-p6").show();
+    });
+
+    $(".popup-closef").click(function(){
+        $(".popup-p6").hide();
+    });
+    // 포에스
+    $(".popup-Btng").click(function(){
+        $(".popup-p7").show();
+    });
+
+    $(".popup-closeg").click(function(){
+        $(".popup-p7").hide();
+    });
+    // 와디즈
+    $(".popup-Btnh").click(function(){
+        $(".popup-p8").show();
+    });
+
+    $(".popup-closeh").click(function(){
+        $(".popup-p8").hide();
+    });
+});
